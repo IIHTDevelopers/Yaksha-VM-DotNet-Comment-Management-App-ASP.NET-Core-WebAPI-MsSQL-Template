c41fceb9a3774eef6815457a18f7d7b9d272ecf2
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-14 11:25:34 +0530

    Add files via upload

---
+++ CommentManagementApp.BusinessLayer/Services/CommentManagementService.cs
@@ -0,0 +1,51 @@
+﻿using CommentManagementApp.BusinessLayer.Interfaces;
+using CommentManagementApp.BusinessLayer.Services.Repository;
+using CommentManagementApp.BusinessLayer.ViewModels;
+using CommentManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentManagementApp.BusinessLayer.Services
+{
+    public class CommentManagementService : ICommentManagementService
+    {
+        private readonly ICommentManagementRepository _repo;
+
+        public CommentManagementService(ICommentManagementRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Comment> CreateComment(Comment employeeComment)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteCommentById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetAllComments()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public async Task<Comment> GetCommentById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public async Task<Comment> UpdateComment(CommentViewModel model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommentManagementApp.BusinessLayer/Services/Repository/CommentManagementRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CommentManagementApp.BusinessLayer.ViewModels;
+using CommentManagementApp.DataLayer;
+using CommentManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CommentManagementApp.BusinessLayer.Services.Repository
+{
+    public class CommentManagementRepository : ICommentManagementRepository
+    {
+        private readonly CommentManagementAppDbContext _dbContext;
+        public CommentManagementRepository(CommentManagementAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Comment> CreateComment(Comment CommentModel)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteCommentById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetAllComments()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public async Task<Comment> GetCommentById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+       
+        public async Task<Comment> UpdateComment(CommentViewModel model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
